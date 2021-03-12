3efeda1c518d870990b9afcf996853eceff1532f
faleirol <faleirol@stu.otc.school.nz>
2021-03-12 13:21:16 +1300

    First task

---
+++ Tasks.py
@@ -0,0 +1,14 @@
+# Team scores
+match_1 = 3
+match_2 = 6
+match_3 = 10
+match_4 = 10
+match_5 = 6
+print("first match:" , match_1)
+print("second match:" , match_2)
+print("third match:" , match_3)
+print("fourth match:" , match_4)
+print("fifth match:" , match_5)
+# Calculating the scores
+total_score = match_1 + match_2 + match_3 + match_4 + match_5
+print("Team final score:" , total_score)
