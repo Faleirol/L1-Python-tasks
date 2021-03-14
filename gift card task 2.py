a9edbb86e8f3be85359c8fdd7cc7fc416395717d
Faleirol <72894672+Faleirol@users.noreply.github.com>
2021-03-15 10:27:21 +1300

    Update gift card task 2.py

--- gift card task 2.py
+++ gift card task 2.py
@@ -2,5 +2,5 @@ card_balance = 50
 game_price = 35
 buy_game = card_balance - game_price
 print("Your balance on your card:" , card_balance)
-print("The game price is $" ,game_price)
+print("The game price is $" , game_price)
 print("Your balance after purchase:" , buy_game)
