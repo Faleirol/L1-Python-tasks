defc83dfdc8f2e1da07af063effd9237d732844c
faleirol <faleirol@stu.otc.school.nz>
2021-03-29 10:56:35 +1300

    saving!!!!

---
+++ Prime-number.py
@@ -0,0 +1,8 @@
+number = int(input("Enter a number"))
+
+if number > 1:
+    for i in range(2, number):
+        if(number % i) == 0:
+            print(f" not a prime number")
+elif:
+     print(f" is a prime number")
