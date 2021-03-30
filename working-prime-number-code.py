a50e998dce236bbf741626698c7c2b600082aad1
Faleirol <72894672+Faleirol@users.noreply.github.com>
2021-03-30 14:32:27 +1300

    Delete working-prime-number-code.py

--- working-prime-number-code.py
+++
@@ -1,9 +0,0 @@
-number = int(input("Enter a number"))
-
-if number > 1:
-    for i in range(2, number):
-        if(number % i) == 0:
-            print(f"{number} not a prime number")
-            break
-else:
-    print(f"{number} is a prime number")
